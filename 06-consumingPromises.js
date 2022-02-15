97e546025a5d15ece1bdaaa77ed0b7b45c3622c2
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-02-15 20:05:15 +0800

    Added codes about Consuming Promises

---
+++ 06-consumingPromises.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+const renderCountry = function (data, className = '') {
+  //Format of restcountries changed, needed to improvise to get first object.
+  const firstLang = Object.values(data.languages)[0];
+  const firstCurr = Object.values(data.currencies)[0];
+
+  const html = `
+    <article class="country ${className}">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name.common}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+              ).toFixed(1)}M people</p>
+              <p class="country__row"><span>🗣️</span>${firstLang}</p>
+              <p class="country__row"><span>💰</span>${firstCurr.name}</p>
+            </div>
+          </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// Consuming Promises
+
+//Fetch - return a promise
+//Then - handle a promise (response)
+//json method - read the data from the response (new promise)
+//Then - access the data
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       //Also an async function - returns a promise
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+//Another implementation
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('portugal');
